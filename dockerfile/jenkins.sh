7a1bf1400dd2b49ef98352f192a27c4a0eca9b66
corentinarnaud <corentin.arnaud9@gmail.com>
2017-04-14 14:40:21 +0200

    test trigger 1

--- dockerfile/jenkins.sh
+++ dockerfile/jenkins.sh
@@ -1,5 +1,4 @@
 docker rm -f jenkins || true
-docker volume create --name CDB_WAR
 docker build -t jenkinsi jenkins/
 docker run -d --name jenkins -v CDB_Volume:/cdb -v WAR_CDB:/cdb_war -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -p 8085:8080 jenkinsi
 sleep 1
